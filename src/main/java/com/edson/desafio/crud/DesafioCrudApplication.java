7e18e82500166f468e35a2b20a219c3bd52537b3
Edson <edsonfilho734@gmail.com>
2021-06-03 21:43:40 -0300

    Criaçcao do projeto

---
+++ src/main/java/com/edson/desafio/crud/DesafioCrudApplication.java
@@ -0,0 +1,12 @@
+package com.edson.desafio.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCrudApplication.class, args);
+	}
+}
---
+++ src/test/java/com/edson/desafio/crud/DesafioCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edson.desafio.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
